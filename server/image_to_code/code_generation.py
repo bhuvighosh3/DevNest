6722b331e36aa38b98934470de9d56a39d0faa21
Vineetttt <vineetchotaliya30@gmail.com>
2023-11-17 15:34:05 +0530

    minor changes to image to code endpoint

--- server/image_to_code/code_generation.py
+++ server/image_to_code/code_generation.py
@@ -1,6 +1,7 @@
 import openai
+import os
 
-openai.api_key = 'openai_key'
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def generate_code_from_caption(caption):
     prompt = f"Generate the code for the described React functional component. It should have React & CSS code too. Name the CSS file & import it. Do not include the image in the code {caption}"
